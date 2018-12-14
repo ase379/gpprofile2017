b71ecd95d6a69e2711ca8ac41b5c3984576728df
unknown <seehusen@medienpunkt.com>
2018-12-14 15:11:26 +0100

    #44: + support for marquee

--- source/gppLoadProgress.pas
+++ source/gppLoadProgress.pas
@@ -16,9 +16,11 @@ type
     procedure FormKeyPress(Sender: TObject; var Key: Char);
     procedure btnCancelLoadClick(Sender: TObject);
   private
+    fMarquee : boolean;
+    procedure setMarquee(const Value: boolean);
     { Private declarations }
   public
-    { Public declarations }
+    property Marquee : boolean read fMarquee write setMarquee;
   end;
 
 var
@@ -31,6 +33,30 @@ implementation
 procedure TfrmLoadProgress.FormKeyPress(Sender: TObject; var Key: Char);
 begin
   if Key = #27 then Hide;
+end;
+
+procedure TfrmLoadProgress.setMarquee(const Value: boolean);
+const
+  PBS_MARQUEE = $08;
+  PBM_SETMARQUEE = WM_USER + 10;
+var
+  LOn : Integer;
+  LNewStyle : NativeInt;
+begin
+  fMarquee := Value;
+  LNewStyle := GetWindowLong(ProgressBar1.Handle, GWL_STYLE);
+  LOn := Integer(FMarquee);
+
+  if fMarquee then
+  begin
+    LNewStyle := LNewStyle Or PBS_MARQUEE;
+  end
+  else
+  begin
+    LNewStyle := LNewStyle AND PBS_MARQUEE;
+  end;
+  SetWindowLong(ProgressBar1.Handle, GWL_STYLE, LNewStyle Or PBS_MARQUEE);
+  SendMessage(ProgressBar1.Handle, PBM_SETMARQUEE, LOn, 40);
 end;
 
 procedure TfrmLoadProgress.btnCancelLoadClick(Sender: TObject);
