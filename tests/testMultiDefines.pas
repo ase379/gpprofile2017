b6c06c7f01ec292f1c9dec180a204a5e1e2c5603
ase379 <seehusen@medienpunkt.com>
2019-07-16 22:17:05 +0200

    67: test code for cascading IF/IFDEF and old IFEND style

--- tests/testMultiDefines.pas
+++ tests/testMultiDefines.pas
@@ -1,8 +1,10 @@
 unit testMultiDefines;
 
+{$LEGACYIFEND}
 interface
 
 procedure TestMultidefinesDummy();
+procedure TestFunctionIfIfdefWithIfEndAndEndIf();
 
 {$IFDEF Win32}
 implementation
@@ -17,6 +19,23 @@ implementation
 // common code
 procedure TestMultidefinesDummy();
 begin
+end;
+
+
+// test for checking if nesting a $IF in a $IFDEF works with Closed $IFEND
+procedure TestFunctionIfIfdefWithIfEndAndEndIf();
+{$ifdef A}
+begin
+{$ELSE}
+{$IF DEFINED(B)}
+begin
+{$ELSE}
+var
+  i : Integer;
+begin
+  i := 0;
+{$IFEND}
+{$IFEND}
 end;
 
 end.
