4a78da88e82c681a651f779aa4f4c77bb324abd1
Andreas Seehusen <Andreas.Seehusen@orgadata.com>
2024-04-04 10:00:03 +0200

    #130
    + added test memory consumption for checking the results

--- tests/testThreads.pas
+++ tests/testThreads.pas
@@ -26,6 +26,7 @@ begin
 end;
 
 procedure TTestThread.Execute;
+var lPointer : Pointer;
 begin
   NameThreadForDebugging('AwesomeThread', self.ThreadID);
   var lOuterScope := gpprof.CreateMeasurePointScope('MP_TestThreadExecuteOuter');
@@ -34,8 +35,10 @@ begin
   self.namethreadfordebugging('AwesomeThread2-SelfNameReplacement', self.ThreadID);
   TThread.NameThreadForDebugging('AwesomeThread3-TThreadReplacement');
   Sleep(1000);
+  GetMem(lPointer, 1024);
   lInnerScope := nil;
   Sleep(1000);
+  FreeMem(lPointer);
   lOuterScope := nil;
   inherited;
 end;
